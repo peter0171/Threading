50a9a962d641233f65832d287e7e65f05ea3e503
peteroneshift <peter@oneshift.com.au>
2015-11-18 20:20:23 +1100

    upload parallel test

---
+++ sln/DataRace/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DataRace
+{
+    class Program
+    {
+        public static int counter = 0;
+        static void Main(string[] args)
+        {
+            Object thisLock = new Object();
+
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            Parallel.For(0, 100000, i =>
+            {
+
+                Thread.Sleep(1);
+
+                #region L
+                Interlocked.Increment(ref counter);
+                //lock (thisLock)
+                //    counter++;
+                #endregion
+                
+                //counter++;
+            });
+            watch.Stop();
+            Console.WriteLine("Seconds Elapsed: " + watch.Elapsed.Seconds);
+            Console.WriteLine(counter.ToString());
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ sln/Tasks/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("frist");
+            Task t = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("I am the first task");
+            });
+
+            var t2 = t.ContinueWith(delegate
+            {
+                //simulate compute intensive
+                Thread.Sleep(10000);
+                return "Devlifestyle";
+            });
+
+            //block1
+            string result = t2.Result;
+            Console.WriteLine("result of second task is: " + result);
+            //end block1
+
+            //block2
+         var t3=t2.ContinueWith(delegate
+                {
+                    Console.WriteLine("Here i am");
+                    return "Devlifestyle222";
+                });
+            Console.WriteLine(t3.Result+"Waiting my task");
+            //end block2
+
+            Console.ReadLine();
+
+        }
+    }
+}
